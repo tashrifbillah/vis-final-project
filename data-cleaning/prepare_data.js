1e5ee41ef50a49d39710a7e7a8964e772513a9e4
Nate Mortensen <nmortensen2@gmail.com>
2020-11-04 20:50:05 -0700

    Add cleaned CSV from NPS

--- data-cleaning/prepare_data.js
+++ data-cleaning/prepare_data.js
@@ -14,19 +14,24 @@ const extractors = {
   location: el => $(el).text().split(/\d/)[0],
   date_established: defaultExtract,
   area: defaultExtract,
-  visitors: el => defaultExtract,
+  visitors: defaultExtract,
   description(el) {
     $('sup', el).remove()
     return $(el).text()
   }
 }
 
+// Iterate over each row
 $('table tbody tr').each(function() {
   const row = {};
+
+  // Iterate through each data point and run through extractor function, add to object
   $('th, td', this).each(function(idx) {
     const header = headers[idx]
     row[header] = extractors[header](this)
   })
+
+  // Append object to outer rows variable
   rows.push(row)
 })
 
