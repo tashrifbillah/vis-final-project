ead98e93bd42f73525339fdeabbddd91ba5294f1
Nate Mortensen <nmortensen2@gmail.com>
2020-11-14 20:59:24 -0700

    Add top 10 parks calculation as part of selecting activities in global filter

--- presentation/script/activitySelect.js
+++ presentation/script/activitySelect.js
@@ -2,20 +2,26 @@ const relations = _.flatten(finalResults.map(r => r.activities.map(a => ({ activ
 const grouped = _.groupBy(relations, 'activity')
 const activityCounts = _.fromPairs(Object.entries(grouped).map(([k, v]) => [k, v.length]), c => c[1], 'desc')
 const allActivities = _.orderBy(_.uniqBy(_.flatten(finalResults.map(r => r.activities)), 'id').map( a => ({ ...a, count: activityCounts[a.id] })), 'count', 'desc')
-let selectedActivities = []
 const eventEmitter = {}
+
+let selectedActivities = []
+let topTenParks = []
 
 new Vue({
     data() {
         return {
             activityCounts,
             allActivities,
-            selectedActivities
+            selectedActivities,
+            parks: finalResults
         }
     },
     computed: {
         activitiesById() {
           return _.fromPairs(this.allActivities.map(a => [a.id, a]))
+        },
+        parksById() {
+            return _.fromPairs(this.parks.map(p => [p.id, p]))
         },
         selectedActivityIds: {
             get() {
@@ -25,6 +31,13 @@ new Vue({
                 selectedActivities = this.selectedActivities = arr.map(id => this.activitiesById[id])
                 $(eventEmitter).trigger('activitiesChanged')
             }
+        },
+        topTenParks() {
+            const allParks = _.flatten(this.selectedActivityIds.map(id => grouped[id]))
+            const counts = _.orderBy(Object.entries(_.countBy(allParks, 'park')), d => d[1], 'desc')
+            console.log(counts)
+
+            return topTenParks = counts.slice(0, 10).map(([park,]) => this.parksById[park])
         }
     }
 }).$mount("#activity-select")
