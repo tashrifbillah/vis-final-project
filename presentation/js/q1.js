ce5ed0d3de5532538b1423ad331daeb51ed96728
Tashrif Billah <tashrifbillah@gmail.com>
2020-11-19 00:10:44 -0500

    show all items as selected

--- presentation/js/q1.js
+++ presentation/js/q1.js
@@ -24,13 +24,15 @@ function initMainPage(dataArray) {
     activities= [...new Set(dataArray[1].map(d=>d.activities.map(r=>r.name)).flat())]
     activities.sort((a,b)=>a>b?1:-1)
     selectedActivities= activities
-    activities.forEach(d=>$("#activitySelect").append(new Option(d,d)))
+    activities.forEach(d=>$("#activitySelect").append(new Option(d,d,selected=true)))
 
     // init map
     myMapVis = new MapVis('map', dataArray[0], dataArray[1]);
 
     $(document).ready(function() {
-        $("#activitySelect").multiselect()
+        $("#activitySelect").multiselect({
+            searchBoxText:'Type here to filter parks by activities ...',
+        })
         $('#activitySelect').click(function(){
             //  $( ":input" ) reads value as space replaced by _, revert that change
             selectedActivities = $( ":input" ).serializeArray().map(d=>d.value.replace(/_/g, " "))
