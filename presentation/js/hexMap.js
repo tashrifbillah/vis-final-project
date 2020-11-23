dedbbec47ad8bd8255aae1d1ace9be67a8aa2d5f
Gordon Wade <gwade@broadinstitute.org>
2020-11-22 21:50:56 -0500

    larger and bold hex map labels

--- presentation/js/hexMap.js
+++ presentation/js/hexMap.js
@@ -176,7 +176,8 @@ class HexMap {
         .attr("y", d => d.label_point[1] + vis.hexRadius/3)
         .attr("class", "state-label")
         .text(d => d.name)
-        .style("font-size", 10)
+        .style("font-size", 12)
+        .style("font-weight", "bold")
         .on("mouseover", function(event, d) {
           vis.toolTip.show(d, this);
         })
