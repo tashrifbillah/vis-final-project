69b7993b20104eefeef22daa9b003de03bd88f4f
Nate Mortensen <nmortensen2@gmail.com>
2020-11-22 19:24:10 -0700

    Add regions to select element

--- presentation/js/eventHandler.js
+++ presentation/js/eventHandler.js
@@ -1,10 +1,16 @@
+selectedRegion = ''
+
 $(eventEmitter).bind('activitiesChanged', function () {
     updateRadar();
     barVis.wrangleData()
-    resultsApp.setRows(topTenParks)
+    resultsApp && resultsApp.setRows(topTenParks)
 })
 
 $(eventEmitter).bind('seasonChanged', function () {
     selectedSeason = $("#season-select").val()
     barVis.wrangleData()
+})
+
+$(eventEmitter).bind('regionChanged', function () {
+    selectedSeason = $("#region-select").val()
 })
